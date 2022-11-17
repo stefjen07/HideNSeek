c49127e333501b41eaf9192e24596a36f3ee9938
Eugene Stsefankou <stefjen@mail.ru>
2022-11-17 21:50:34 +0300

    Initial Commit

---
+++ HideNSeek/HideNSeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  HideNSeekApp.swift
+//  HideNSeek
+//
+//  Created by Евгений on 17.11.22.
+//
+
+import SwiftUI
+
+@main
+struct HideNSeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
